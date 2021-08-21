8db46dfaff4bd06df6a188313f5c5dc5db94a8aa
Tobi Kuyoro <tobikuyo@gmail.com>
2021-08-21 19:44:09 +0100

    Changes to docker-compose and configuration files

--- server-mongo/database/config.js
+++ server-mongo/database/config.js
@@ -5,12 +5,7 @@ const dbName = process.env.DB_NAME;
 
 const init = async () => {
 	const client = await MongoClient.connect(dbConnection);
-	const db = client.db(dbName);
-	db.createCollection('users', (err, res) => {
-		if (err) throw err;
-	});
-
-	return db;
+	return client.db(dbName);
 };
 
 module.exports = init;
