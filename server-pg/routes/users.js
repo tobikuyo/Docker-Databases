58798def9b4b35717e7651571db77dc276ea4a25
Tobi Kuyoro <tobikuyo@gmail.com>
2021-08-23 00:02:14 +0100

    Routes to find and update a user

--- server-pg/routes/users.js
+++ server-pg/routes/users.js
@@ -12,6 +12,17 @@ router.get('/', async (req, res) => {
 	}
 });
 
+router.get('/:id', async (req, res) => {
+	const { id } = req.params;
+
+	try {
+		const user = await User.find(id);
+		res.status(200).json({ data: user });
+	} catch (error) {
+		res.status(404).json({ message: `There is no user with the id '${id}'` });
+	}
+});
+
 router.post('/', async (req, res) => {
 	try {
 		const { username, location } = req.body;
@@ -21,6 +32,18 @@ router.post('/', async (req, res) => {
 		res.status(400).json({
 			message: 'There was an error with adding this new user'
 		});
+	}
+});
+
+router.patch('/:id', async (req, res) => {
+	const { id } = req.params;
+
+	try {
+		const user = await User.find(parseInt(id));
+		const updatedUser = await user.update(req.body);
+		res.status(200).json({ data: updatedUser });
+	} catch (error) {
+		res.status(404).json({ message: `There is no user with the id '${id}'` });
 	}
 });
 
