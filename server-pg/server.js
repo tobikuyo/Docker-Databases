d1f87dda9d5701ede8bd5cdb6996ed0ea8d9ff2a
Tobi Kuyoro <tobikuyo@gmail.com>
2021-08-22 21:39:01 +0100

    Connect to app

---
+++ server-pg/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const userRoutes = require('./routes/users');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/users', userRoutes);
+
+module.exports = app;
